74cad92b17c1d78bb3974f7a5829239284e31bb6
gruijter <gruijter@hotmail.com>
2026-06-17 11:37:19 +0200

    Support _EU environment variables and add fallbacks for keys when not specified

--- app.js
+++ app.js
@@ -79,10 +79,10 @@ module.exports = class SungrowApp extends OAuth2App {
     const buildConfig = (region) => {
       const suffix = region.configId.toUpperCase();
       const cloudId = Homey.env[`CLOUD_ID_${suffix}`] || region.cloudId;
-      const applicationId = Homey.env[`APPLICATION_ID_${suffix}`] || region.applicationId;
+      const applicationId = Homey.env[`APPLICATION_ID_${suffix}`] || region.applicationId || Homey.env.APPLICATION_ID || Homey.env.APPLICATION_ID_EU;
       return {
-        clientId: Homey.env[`CLIENT_ID_${suffix}`] || Homey.env.CLIENT_ID,
-        clientSecret: Homey.env[`CLIENT_SECRET_${suffix}`] || Homey.env.CLIENT_SECRET,
+        clientId: Homey.env[`CLIENT_ID_${suffix}`] || Homey.env.CLIENT_ID || Homey.env.CLIENT_ID_EU,
+        clientSecret: Homey.env[`CLIENT_SECRET_${suffix}`] || Homey.env.CLIENT_SECRET || Homey.env.CLIENT_SECRET_EU,
         apiUrl: region.apiUrl,
         tokenUrl: `${region.apiUrl}/openapi/apiManage/token`,
         authorizationUrl: `https://${region.authHost}/#/authorized-app?cloudId=${cloudId}&applicationId=${applicationId}&redirectUrl=${REDIRECT_URL}`,
